54d8c80eeb25c1b4f4405250a13aaa12435e18ec
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-09-02 11:51:32 +0530

    telescope changes + buffer keybinds + lualine changes

--- lua/sama/set.lua
+++ lua/sama/set.lua
@@ -16,10 +16,14 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.clipboard="unnamedplus"
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
 
+-- Search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true -- Case insensitive searching
+vim.opt.smartcase = true -- Case-sensitive if there is an uppercase letter
 
 vim.opt.termguicolors = true
 
@@ -27,10 +31,6 @@ vim.opt.scrolloff = 8
 --vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "0"
-
-
-
