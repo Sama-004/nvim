afeec019369f77a8a44c5a24cd25d7dde4b214b4
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-09-02 09:47:06 +0530

    added telescope changes

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,22 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string( {search = vim.fn.input("Grep > ") });
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<Leader>ps", builtin.live_grep, {})
 
-end)
+-- vim.keymap.set("n", "<leader>ps", function()
+-- 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+-- end)
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-j>"] = require("telescope.actions").move_selection_next,
+				["<C-k>"] = require("telescope.actions").move_selection_previous,
+			},
+			n = {
+				["j"] = require("telescope.actions").move_selection_next,
+				["k"] = require("telescope.actions").move_selection_previous,
+			},
+		},
+	},
+})
--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 require("sama")
 
-vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true })
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap("i", "kj", "<Esc>", { noremap = true })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
 --vim.api.nvim_set_keymap('v', 'kj', '<Esc>', { noremap = true })
 --vim.api.nvim_set_keymap('v', 'jk', '<Esc>', { noremap = true })
-vim.api.nvim_set_keymap('v', '<Leader>', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap("v", "<Leader>", "<Esc>", { noremap = true })
 -- Map '(' to '()' and move cursor inside the parentheses
-vim.api.nvim_set_keymap('i', '(', '()<Left>', { noremap = true })
-vim.api.nvim_set_keymap('i', '{', '{}<Left>' ,   { noremap = true })
-vim.api.nvim_set_keymap('i', '[', '[]<Left>' ,   { noremap = true })
+vim.api.nvim_set_keymap("i", "(", "()<Left>", { noremap = true })
+vim.api.nvim_set_keymap("i", "{", "{}<Left>", { noremap = true })
+vim.api.nvim_set_keymap("i", "[", "[]<Left>", { noremap = true })
